4b67cfc7f1dd63a57f061587103e0a9de7910756
kiavaco <info@kiava.co>
2019-05-09 10:54:57 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,31 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "Administrative",
+    "Fun",
+    "GroupManager",
+    "Msg-Checks",
+    "SetUp-Plugins",
+    "Core",
+    "Helper-Api",
+    "Help-Api",
+    "Helper-Top",
+    "test",
+    "Mutetime"
+  },
+  enabled_plugins_api = {
+    "Helper-Api",
+    "Helper-Top",
+    "Help-Api"
+  },
+  sudo_users = {
+    464555636,
+    605046353,
+    679719822,
+    521307846,
+    97476012
+  }
+}
+return _
+end
